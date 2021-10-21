b405045290e52f9a8e576a95d2d6e37ade111053
DaniilKochergin <kochergin-2000@mail.ru>
2021-10-22 00:32:42 +0300

    refactor all servlets to use DAO and abstract class

--- src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -1,37 +1,27 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
-import javax.servlet.http.HttpServlet;
+import ru.akirakozov.sd.refactoring.dao.ProductDAO;
+import ru.akirakozov.sd.refactoring.models.Product;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
-public class AddProductServlet extends HttpServlet {
+public class AddProductServlet extends ProductServlet {
+
+    public AddProductServlet(ProductDAO productDAO) {
+        super(productDAO);
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String name = request.getParameter("name");
         long price = Long.parseLong(request.getParameter("price"));
 
-        try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                String sql = "INSERT INTO PRODUCT " +
-                        "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-                Statement stmt = c.createStatement();
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        productDAO.insert(new Product(name, price));
 
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("OK");
     }
 }
--- src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
@@ -1,43 +1,30 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
-import javax.servlet.http.HttpServlet;
+import ru.akirakozov.sd.refactoring.dao.ProductDAO;
+import ru.akirakozov.sd.refactoring.models.Product;
+
+import java.util.List;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
-public class GetProductsServlet extends HttpServlet {
+public class GetProductsServlet extends ProductServlet {
+
+    public GetProductsServlet(ProductDAO productDAO) {
+        super(productDAO);
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                Statement stmt = c.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-
-                while (rs.next()) {
-                    String  name = rs.getString("name");
-                    int price  = rs.getInt("price");
-                    response.getWriter().println(name + "\t" + price + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-                stmt.close();
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PrintWriter printerWriter = response.getWriter();
+        List<Product> products = productDAO.getAll();
+        printerWriter.println("<html><body>");
+        for (Product product : products) {
+            printerWriter.println(product.toHTML());
         }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+        printerWriter.println("</body></html>");
     }
 }
