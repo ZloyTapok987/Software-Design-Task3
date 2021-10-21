9b6e752e317ff135554f37431a09a400187865ac
DaniilKochergin <kochergin-2000@mail.ru>
2021-10-22 00:19:37 +0300

    create model for product

---
+++ src/main/java/ru/akirakozov/sd/refactoring/models/Product.java
@@ -0,0 +1,51 @@
+package ru.akirakozov.sd.refactoring.models;
+
+import java.util.Objects;
+
+public class Product {
+
+    private final String name;
+    private final long price;
+
+    public Product(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String toHTML() {
+        return name + "\t" + price + "</br>";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Product product = (Product) object;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product {" +
+                "  name: " + name +
+                " price: " + price +
+                "}";
+    }
+}
